9ce104bd86834b7c44c634db2e6341a4a8672fa2
youngsunrise <2495212386@qq.com>
2025-02-07 17:06:56 +0800

    3.setnx实现

--- src/main/java/com/hellobike/skyvoucher/controller/UserVoucherController.java
+++ src/main/java/com/hellobike/skyvoucher/controller/UserVoucherController.java
@@ -22,8 +22,10 @@ public class UserVoucherController {
     //限时秒杀券领取入口
     @PostMapping("getVoucher/{user_id}/{voucher_id}")
     public Result getSecKillVoucher(@PathVariable("user_id") Long userId, @PathVariable("voucher_id") Long voucherId) {
-        return Result.ok(voucherSeckillService.getSecKillVoucher(userId, voucherId));
+        return voucherSeckillService.getSecKillVoucher(userId, voucherId);
     }
+
+
 
     //添加秒杀优惠劵接口
     @PostMapping("addSecKillVoucher")
--- src/main/java/com/hellobike/skyvoucher/utils/SimpleRedisLock.java
+++ src/main/java/com/hellobike/skyvoucher/utils/SimpleRedisLock.java
@@ -3,8 +3,11 @@ package com.hellobike.skyvoucher.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +19,11 @@ public class SimpleRedisLock {
     private String lockName;
     private StringRedisTemplate stringRedisTemplate;
     private static final String ID_PREFIX = UUID.randomUUID().toString();
+    private static final DefaultRedisScript<Long> SRL_UNLOCK = new DefaultRedisScript<>();
+    static {
+        SRL_UNLOCK.setResultType(Long.TYPE);
+        SRL_UNLOCK.setLocation(new ClassPathResource("SRL_UNLOCK.Lua"));
+    }
 
     public boolean tryLock(long timeOutSec) {
         //准备线程标识，用于标注锁持有者
@@ -33,11 +41,18 @@ public class SimpleRedisLock {
 
     public void unlock() {
         //判断线程标识后，释放锁
-        String mark = stringRedisTemplate.opsForValue().get(RedisConsts.REDIX_SETNX_LOCK_PREFIX + lockName);
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        //判断当前线程标识，与当前锁持有者标识是否一致
-        if (mark.equals(threadId)) {
-            stringRedisTemplate.delete(RedisConsts.REDIX_SETNX_LOCK_PREFIX + lockName);
-        }
+//        String mark = stringRedisTemplate.opsForValue().get(RedisConsts.REDIX_SETNX_LOCK_PREFIX + lockName);
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        //判断当前线程标识，与当前锁持有者标识是否一致
+//        if (mark.equals(threadId)) {
+//            stringRedisTemplate.delete(RedisConsts.REDIX_SETNX_LOCK_PREFIX + lockName);
+//        }
+
+        //使用Lua脚本实现 1.判断锁标识 2.释放锁 两个步骤的原子化
+        stringRedisTemplate.execute(
+                SRL_UNLOCK,
+                Collections.singletonList(RedisConsts.REDIX_SETNX_LOCK_PREFIX + lockName),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
     }
 }
